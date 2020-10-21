fd3d4ff700677b5a00987cd39065e910a4615b20
Bifeldy <bias.astho@gmail.com>
2020-10-21 13:43:12 +0900

    ✔ Handle Error

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const readline = require("readline");
-const { Client, RichEmbed, TextChannel } = require('discord.js');
+const { Client, RichEmbed, TextChannel, version } = require('discord.js');
 
 const client = new Client();
 
 // Go To Discord, Open DevTools (Ctrl + Shift + I), Under `Application` -> `Local Storage` -> `token`
 // Put Your Token Here As String Into `DISCORD_LOGIN_TOKEN` Variable
-// Example let DISCORD_LOGIN_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-let DISCORD_LOGIN_TOKEN = null;
+let DISCORD_LOGIN_TOKEN = process.env.DISCORD_TOKEN || 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' || null;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -14,54 +13,54 @@ const rl = readline.createInterface({
 });
 
 client.on("ready", () => {
-  console.log(`✨ Client Logged In As :: ${client.user.username}#${client.user.discriminator}`);
+  console.log(`✨ User Logged In As :: ${client.user.username}#${client.user.discriminator}`);
+  console.log(`🎉 Discord API & Token :: ${version} :: ${client.token}`);
 });
 
 client.on("message", async message => {
-  console.log(`[${message.guild.name}] [${message.channel.name}] [${message.author.username}#${message.author.discriminator}] ${message.content}`);
+  try {
+    console.log(`[${message.guild.name}] [${message.channel.name}] [${message.author.username}#${message.author.discriminator}] ${message.content}`);
 
-  // Example :: Use `!quote <discord_msg_url> <ReplayMsgThat CanAlsoHave SpaceCharacter AndThisIsNotRequired>`
-  // !quote https://discord.com/channels/281068107974443009/281068107974443009/767639372023201792 Reply Message Text Here
-  if (message.content.startsWith('!quote') && message.author.id === client.user.id) {
-    const captureRegex = /!quote (https:\/\/discord.com\/channels\/([0-9]+)\/([0-9]+)\/([0-9]+))(.*$)/gi;
-    const captureResult = captureRegex.exec(message.content);
-    const [rawInput, MessageUrl, serverId, channelId, messageId, textReply] = captureResult;
-    if (rawInput && MessageUrl && serverId && channelId && messageId) {
-      const quotedServer = await client.guilds.get(serverId);
-      const quotedChannel = await quotedServer.channels.get(channelId);
-      const textChannel = new TextChannel(quotedServer, quotedChannel);
-      const quotedMessage = await textChannel.fetchMessage(messageId);
-      embedQuote(MessageUrl, message.channel, quotedMessage, textReply);
-      message.delete().catch(console.error);
+    // Example :: Use `!quote <discord_msg_url> <ReplayMsgThat CanAlsoHave SpaceCharacter AndThisIsNotRequired>`
+    // !quote https://discord.com/channels/281068107974443009/281068107974443009/767639372023201792 Reply Message Text Here
+    if (message.content.startsWith('!quote') && message.author.id === client.user.id) {
+      const captureRegex = /!quote (https:\/\/discord.com\/channels\/([0-9]+)\/([0-9]+)\/([0-9]+))(.*$)/gi;
+      const captureResult = captureRegex.exec(message.content);
+      const [rawInput, quotedMessageUrl, serverId, channelId, messageId, textReply] = captureResult;
+      if (rawInput && quotedMessageUrl && serverId && channelId && messageId) {
+        const quotedServer = await client.guilds.get(serverId);
+        const quotedChannel = await quotedServer.channels.get(channelId);
+        const textChannel = new TextChannel(quotedServer, quotedChannel);
+        const quotedMessage = await textChannel.fetchMessage(messageId);
+        const messageEmbed = new RichEmbed()
+          .setColor(quotedMessage.member.displayColor)
+          .setAuthor(quotedMessage.author.username, quotedMessage.author.avatarURL, quotedMessageUrl)
+          .setDescription(quotedMessage.content)
+          .setTimestamp(quotedMessage.createdTimestamp)
+          .setFooter(`#${quotedMessage.channel.name} @ ${quotedMessage.channel.guild.name}`);
+        const messageSend = await message.channel.send(textReply, messageEmbed);
+        const messageDelete = await message.delete();
+      }
     }
+
+    // TODO :: You Can Add Other Self Bot Command Here
+
+  } catch (err) {
+    console.log(err);
   }
-
-  // TODO :: You Can Add Other Self Bot Command Here
-
 });
 
-function embedQuote(MessageUrl, channel, quotedMessage, textReply) {
-  const messageEmbed = new RichEmbed()
-    .setColor(quotedMessage.member.displayColor)
-    .setAuthor(quotedMessage.author.username, quotedMessage.author.avatarURL, MessageUrl)
-    .setDescription(quotedMessage.content)
-    .setTimestamp(quotedMessage.createdTimestamp)
-    .setFooter(`#${quotedMessage.channel.name} @ ${quotedMessage.channel.guild.name}`);
-  channel.send(messageEmbed);
-  if (textReply) {
-    channel.send(textReply);
+async function start() {
+  try {
+    const login = await client.login(DISCORD_LOGIN_TOKEN);
+  } catch (err) {
+    console.log(err);
+    DISCORD_LOGIN_TOKEN = null;
+    rl.question('🏹 Input User Token :: ', (token) => {
+      DISCORD_LOGIN_TOKEN = token;
+      start();
+    });
   }
 }
 
-function start() {
-  client.login(DISCORD_LOGIN_TOKEN);
-}
-
-if (!DISCORD_LOGIN_TOKEN) {
-  rl.question('🏹 Input User Token :: ', (token) => {
-    DISCORD_LOGIN_TOKEN = token;
-    start();
-  });
-} else {
-  start();
-}
+start();
