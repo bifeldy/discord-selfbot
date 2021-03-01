7b9882ee2e4f212ac1e73b9ceee257de9f1c5c9b
Bifeldy <bias.astho@gmail.com>
2021-03-01 21:17:19 +0900

    ✔ Tagged By Someone And Reply With Emoji `@me Ping`

--- index.js
+++ index.js
@@ -19,11 +19,68 @@ client.on("ready", () => {
 
 client.on("message", async message => {
   try {
-    console.log(`[${message.guild.name}] [${message.channel.name}] [${message.author.username}#${message.author.discriminator}] ${message.content}`);
+
+    // Logging Message Only From What I Sent Only
+    if (message.author.id === client.user.id) {
+      console.log(`[${message.guild.name}] [${message.channel.name}] [${message.author.username}#${message.author.discriminator}] ${message.content}`);
+    }
+
+    // Auto Reply If Someone Tag Me With Sticker / Emoji Ping Angry
+    if (
+      (
+        message.content.includes(`<@${client.user.id}>`) ||
+        message.content.includes(`<@!${client.user.id}>`)
+      ) && message.author.id !== client.user.id
+    ) {
+      const emojiPing = [
+        '<a:angeryping:764216711943553074>',
+        '<:PeepoPing:804619370209869874>',
+        '<:pingblob:764216893552459796>',
+        '<a:chaikaping:529354369750138881>',
+        '<a:AniPing:581882192175562752>',
+        '<:GWnoneAngryPing:615559148167364608>',
+        '<a:LumiPing:749361418385489970>',
+        '<:yutagme:518103195474722826>'
+      ];
+      if (message.content.length === `<@${client.user.id}>`.length || message.content.length === `<@!${client.user.id}>`.length) {
+        const _ = await message.channel.send(emojiPing[Math.floor(Math.random() * emojiPing.length)]);
+      } else {
+        const quotedServer = await client.guilds.get(message.guild.id);
+        const quotedChannel = await quotedServer.channels.get(message.channel.id);
+        const textChannel = new TextChannel(quotedServer, quotedChannel);
+        const quotedMessage = await textChannel.fetchMessage(message.id);
+        const messageEmbed = new RichEmbed();
+        messageEmbed.setColor(quotedMessage.member.displayColor);
+        messageEmbed.setAuthor(
+          `${quotedMessage.author.username}#${quotedMessage.author.discriminator}`,
+          quotedMessage.author.avatarURL,
+          `https://discord.com/channels/${message.guild.id}/${message.channel.id}/${message.id}`
+        );
+        messageEmbed.setDescription(quotedMessage.content);
+        if (quotedMessage.attachments) {
+          const attachment = quotedMessage.attachments.entries().next().value;
+          if (attachment) {
+            const [_, attachmentContent] = attachment;
+            if (attachmentContent.height && attachmentContent.width) {
+              messageEmbed.setImage(attachmentContent.url);
+            } else {
+              const totalFileSize = attachmentContent.filesize.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+              messageEmbed.addField(
+                attachmentContent.filename,
+                `File Size :: ${totalFileSize} Bytes`
+              );
+            }
+          }
+        }
+        messageEmbed.setTimestamp(quotedMessage.createdTimestamp);
+        messageEmbed.setFooter(`#${quotedMessage.channel.name} @ ${quotedMessage.channel.guild.name}`);
+        const _ = await message.channel.send(emojiPing.join(' '), messageEmbed);
+      }
+    }
 
     // Example :: Use `!quote <discord_msg_url> <ReplayMsgThat CanAlsoHave SpaceCharacter AndThisIsNotRequired>`
     // !quote https://discord.com/channels/281068107974443009/281068107974443009/767639372023201792 Reply Message Text Here
-    if (message.content.startsWith('!quote') && message.author.id === client.user.id) {
+    else if (message.content.startsWith('!quote') && message.author.id === client.user.id) {
       const _ = await message.delete();
       let captureRegex = null;
       if (message.content.startsWith('!quote https://discordapp.com')) {
@@ -40,7 +97,11 @@ client.on("message", async message => {
         const quotedMessage = await textChannel.fetchMessage(messageId);
         const messageEmbed = new RichEmbed();
         messageEmbed.setColor(quotedMessage.member.displayColor);
-        messageEmbed.setAuthor(quotedMessage.author.username, quotedMessage.author.avatarURL, quotedMessageUrl);
+        messageEmbed.setAuthor(
+          `${quotedMessage.author.username}#${quotedMessage.author.discriminator}`,
+          quotedMessage.author.avatarURL,
+          quotedMessageUrl
+        );
         messageEmbed.setDescription(quotedMessage.content);
         if (quotedMessage.attachments) {
           const attachment = quotedMessage.attachments.entries().next().value;
@@ -49,18 +110,16 @@ client.on("message", async message => {
             if (attachmentContent.height && attachmentContent.width) {
               messageEmbed.setImage(attachmentContent.url);
             } else {
+              const totalFileSize = attachmentContent.filesize.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
               messageEmbed.addField(
                 attachmentContent.filename,
-                `File Size :: ${attachmentContent.filesize.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")} Bytes`
+                `File Size :: ${totalFileSize} Bytes`
               );
             }
           }
         }
         messageEmbed.setTimestamp(quotedMessage.createdTimestamp);
-        messageEmbed.setFooter(
-          `${quotedMessage.channel.guild.name} in #${quotedMessage.channel.name}`,
-          `https://cdn.discordapp.com/icons/${quotedMessage.channel.guild.id}/${quotedMessage.channel.guild.icon}`
-        );
+        messageEmbed.setFooter(`#${quotedMessage.channel.name} @ ${quotedMessage.channel.guild.name}`);
         const _ = await message.channel.send(textReply, messageEmbed);
       }
     }
